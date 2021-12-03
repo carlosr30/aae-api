b069dc8afef699204b806ad9180669adf8dfbbce
Carlos Resultay <resultaycarlos@gmail.com>
2021-12-03 12:45:12 +0800

    Appetiser Apps Exam

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 class CalendarController extends Controller
 {
+    /**
+     * I only have one web server as of the moment, so what I did is copied
+     * the vue.js client build in this calendar view so that I can deploy
+     * both the api and the client in a single server.
+     */
     public function calendar() {
         return view('calendar');
     }
