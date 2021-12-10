1412bc8da53ef3783f51a8e690235dc8e7947e2d
Carlos Resultay <resultaycarlos@gmail.com>
2021-12-10 11:29:13 +0800

    Appetiser Apps Exam

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,19 +6,19 @@
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="icon" href="/favicon.ico">
     <title>Basic Calendar</title>
-    <link href="/css/app.baa293d8.css" rel="preload" as="style">
+    <link href="/css/app.bd87c521.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.98a879dd.css" rel="preload" as="style">
-    <link href="/js/app.4517a3fd.js" rel="preload" as="script">
+    <link href="/js/app.334d3ee9.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.7bba6b7e.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.98a879dd.css" rel="stylesheet">
-    <link href="/css/app.baa293d8.css" rel="stylesheet">
+    <link href="/css/app.bd87c521.css" rel="stylesheet">
 </head>
 <body>
     <noscript>
-        <strong>We're sorry but Basic Calendar doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+        <strong>We're sorry but appetiser-apps-exam doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
     </noscript>
     <div id="app"></div>
     <script src="/js/chunk-vendors.7bba6b7e.js"></script>
-    <script src="/js/app.4517a3fd.js"></script>
+    <script src="/js/app.334d3ee9.js"></script>
 </body>
 </html>
